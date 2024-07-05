a10488f9acea992361845dc773cc0379f8541dfa
jondhalevinayak <jondhalevinayak2@gmail.com>
2024-07-05 02:07:58 -0500

    first commit

---
+++ src/main/java/com/testSevice/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.testSevice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("vinayak")
+                .password("Welcome1#a")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
+                        //.requestMatchers("/api/**").permitAll()
+                        .anyRequest().authenticated()
+                ).headers((headers) -> headers.disable())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+}
